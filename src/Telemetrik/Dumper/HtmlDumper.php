9ab925ad0d947549633488ff76245c0dd201dc2a
Piotr Kacała <pkacala@gog.com>
2014-12-31 16:58:52 +0100

    Added white-space attribute

--- src/Telemetrik/Dumper/HtmlDumper.php
+++ src/Telemetrik/Dumper/HtmlDumper.php
@@ -6,7 +6,7 @@ class HtmlDumper implements Dumper
 {
     public static function dump($data)
     {
-        echo '<pre style="padding:10px;font-family:Consolas,Menlo,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New,monospace,serif;margin-bottom:10px;background-color:#eee">';
+        echo '<pre style="white-space: pre;padding:10px;font-family:Consolas,Menlo,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New,monospace,serif;margin-bottom:10px;background-color:#eee">';
         SimpleDumper::dump($data);
         echo '</pre>';
     }
